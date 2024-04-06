fd9853baa125c9bed8d12ba66fa75d67e8bd0dcb
Mohammed Adil <66908713+mohammed-xp@users.noreply.github.com>
2024-04-06 11:09:26 +0200

    Done title ui in quiz view

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -3,9 +3,15 @@ import 'package:flutter/material.dart';
 
 abstract class Styles{
 
+  // headline4
   static const textStyle34 = TextStyle(
     fontSize: 34,
     fontWeight: FontWeight.bold,
+  );
+
+  static const textStyle24 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w400,
   );
 
   static const textStyle14 = TextStyle(
--- lib/features/quiz/presentation/views/widgets/quiz_view_body.dart
+++ lib/features/quiz/presentation/views/widgets/quiz_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/constants.dart';
+import 'package:quiz_app/core/utils/styles.dart';
 import 'package:quiz_app/features/quiz/presentation/views/widgets/progress_bar.dart';
 
 class QuizViewBody extends StatelessWidget {
@@ -7,11 +8,34 @@ class QuizViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          ProgressBar()
+          const ProgressBar(),
+          const SizedBox(height: kDefaultPadding,),
+          Text.rich(
+            TextSpan(
+              text: 'Question 1',
+              style: Styles.textStyle34.copyWith(
+                fontWeight: FontWeight.w400,
+                color: kSecondaryColor,
+              ),
+              children: [
+                TextSpan(
+                  text: '/10',
+                  style: Styles.textStyle24.copyWith(
+                    color: kSecondaryColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Divider(
+            thickness: 1.5,
+          ),
+          const SizedBox(height: kDefaultPadding,),
         ],
       ),
     );
